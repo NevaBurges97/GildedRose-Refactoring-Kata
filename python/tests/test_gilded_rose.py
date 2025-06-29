8093292169050710ffff8fcdcffe4ebf0c5d82bb
Neva Burges <nevaburges@hotmail.com>
2025-06-29 23:32:43 +0530

    added tests for ordinary items

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -10,7 +10,34 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("Elixir of the Mongoose", items[0].name)
-
         
+    def test_update_ordinary_item_before_sell_by_date(self):
+        items = [Item("Elixir of the Mongoose", 10, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(gilded_rose.items[0].sell_in, 9)
+        self.assertEqual(gilded_rose.items[0].quality, 19)
+        
+    def test_update_ordinary_item_1_day_before_sell_by_date(self):
+        items = [Item("Elixir of the Mongoose", 1, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(gilded_rose.items[0].sell_in, 0)
+        self.assertEqual(gilded_rose.items[0].quality, 19)
+
+    def test_update_ordinary_item_on_sell_by_date(self):
+        items = [Item("Elixir of the Mongoose", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(gilded_rose.items[0].sell_in, -1)
+        self.assertEqual(gilded_rose.items[0].quality, 18)
+
+    def test_update_ordinary_item_after_sell_date(self):
+        items = [Item("Elixir of the Mongoose", -1, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(gilded_rose.items[0].sell_in, -2)
+        self.assertEqual(gilded_rose.items[0].quality, 18)
+
 if __name__ == '__main__':
     unittest.main()
