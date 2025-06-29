8a8531ec595a2c96f5bc74e2d9a60879dbd8285c
Neva Burges <nevaburges@hotmail.com>
2025-06-30 00:57:19 +0530

    rearrage nested conditions in items past sell by check to check one item type per condition

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -22,18 +22,14 @@ class GildedRose(object):
             elif item.name == "Aged Brie":
                 self.increase_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = 0
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                if item.name == "Aged Brie":
+                    self.increase_quality(item)
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
+                elif item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0:
+                    item.quality -= 1
 
 
 class Item:
