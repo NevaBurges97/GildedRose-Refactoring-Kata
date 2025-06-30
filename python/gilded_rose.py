17ede6570b2723d18d4d33587f02341cf834e7ef
Neva Burges <nevaburges@hotmail.com>
2025-07-01 00:35:51 +0530

    extract different item types into their own classes

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,38 +1,5 @@
 # -*- coding: utf-8 -*-
-
-class GildedRose(object):
-
-    def __init__(self, items):
-        self.items = items
-
-    def increase_quality(self, item):
-        if item.quality < 50:
-            item.quality += 1
-
-    def decrease_quality(self, item):
-        if item.quality > 0:
-            item.quality -= 1
-
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in -= 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                self.increase_quality(item)
-                if item.sell_in < 10:
-                    self.increase_quality(item)
-                if item.sell_in < 5:
-                    self.increase_quality(item)
-                if item.sell_in < 0:
-                    item.quality = 0
-            elif item.name == "Aged Brie":
-                self.increase_quality(item)
-                if item.sell_in < 0:
-                    self.increase_quality(item)
-            elif item.name != "Sulfuras, Hand of Ragnaros":
-                self.decrease_quality(item)
-                if item.sell_in < 0:
-                    self.decrease_quality(item)
+from abc import abstractmethod
 
 
 class Item:
@@ -43,3 +10,72 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+class BaseItem(Item):
+    def __init__(self, item: Item):
+        super().__init__(item.name, item.sell_in, item.quality)
+
+    @abstractmethod
+    def update_quality():
+        pass
+    
+    def decrease_quality(self):
+        if self.quality > 0:
+            self.quality -= 1
+
+    def increase_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+
+
+class OrdinaryItem(BaseItem):
+    def update_quality(self):
+        self.sell_in -= 1
+        self.decrease_quality()
+        if self.sell_in < 0:
+            self.decrease_quality()
+
+
+class BackstagePasses(BaseItem):
+    def update_quality(self):
+        self.sell_in -= 1
+        self.increase_quality()
+        if self.sell_in < 10:
+            self.increase_quality()
+        if self.sell_in < 5:
+            self.increase_quality()
+        if self.sell_in < 0:
+            self.quality = 0
+
+
+class AgedBrie(BaseItem):
+    def update_quality(self):
+        self.sell_in -= 1
+        self.increase_quality()
+        if self.sell_in < 0:
+            self.increase_quality()
+
+
+class Sulfuras(BaseItem):
+    def update_quality(self):
+        pass
+
+
+class GildedRose(object):
+    ITEM_CLASSES = {
+        "Aged Brie": AgedBrie,
+        "Backstage passes to a TAFKAL80ETC concert": BackstagePasses,
+        "Sulfuras, Hand of Ragnaros": Sulfuras
+    }
+
+    def __init__(self, items: list[Item]):
+        self.items: list[BaseItem] = [self.create_item(item) for item in items]
+
+    def create_item(self, item: Item):
+        item_class = self.ITEM_CLASSES.get(item.name, OrdinaryItem)
+        return item_class(item)
+    
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
